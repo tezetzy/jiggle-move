c530f9904988598aae9269ddbb8d4966b1d6a418
tezetzy <firlanrizqulloh@gmail.com>
2025-06-14 21:18:19 +0700

    Update Peds.h

--- Peds.h
+++ Peds.h
@@ -37,7 +37,7 @@ enum ePedType
     PED_TYPE_GANG12,
 };
 
-struct CPed
+/*struct CPed
 {
     DWORD vtable;
     DWORD m_dwUnk1;
@@ -46,6 +46,22 @@ struct CPed
     CTaskManager* m_pTaskMgr;
     CWeapon m_aWeapons[13];   // jika kamu pakai array
     BYTE m_nActiveWeaponSlot;
+};*/
+struct CPed
+{
+    uint32_t vtable;              // 0x000
+    char _pad1[0x20 - 0x4];       // padding sampai offset 0x20
+    uint32_t pedType;             // 0x020
+    char _pad2[0x318 - 0x24];     // padding sampai senjata
+
+    CWeapon m_aWeapons[13];       // 0x318
+    uint8_t m_nActiveWeaponSlot;  // 0x394
+    char _pad3[3];                // align to 4-byte
+
+    CWeapon* GetActiveWeapon()
+    {
+        return &m_aWeapons[m_nActiveWeaponSlot];
+    }
 };
 
 struct CPlayerPed : public CPed
