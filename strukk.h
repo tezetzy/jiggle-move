bc7dcd003f865a2592554185ff8b9b1f0033e642
tezetzy <firlanrizqulloh@gmail.com>
2025-06-14 20:31:45 +0700

    Update strukk.h

--- strukk.h
+++ strukk.h
@@ -98,6 +98,8 @@ struct CVehicle
     // Tambah sesuai kebutuhan
 };
 
+
+
 // Rw-related wrapper
 struct CEntity
 {
@@ -109,4 +111,48 @@ struct CEntity
 struct CWorld
 {
     CObject* pObjects[350];
+};
+struct CWeapon
+{
+    DWORD m_nType;         // jenis senjata (mis. WEAPON_PISTOL)
+    DWORD m_nState;        // state senjata (idle, reload, dll)
+    DWORD m_nAmmoInClip;
+    DWORD m_nTotalAmmo;
+    float m_fTargetRange;
+    float m_fAccuracy;
+    CVector m_vecFireOffset; // offset peluru keluar
+};
+struct CPlayerPed : public CPed
+{
+    DWORD m_nPlayerNumber;
+    bool m_bCanBeDamaged;
+    float m_fStamina;
+    // Tambahkan lain sesuai kebutuhan
+};
+struct CWeaponInfo
+{
+    DWORD m_nWeaponFireType;
+    DWORD m_nWeaponType;
+    DWORD m_nSlot;
+    DWORD m_dwFlags;
+    float m_fTargetRange;
+    float m_fAccuracy;
+    float m_fMoveSpeed;
+    float m_fAnimLoopStart;
+    float m_fAnimLoopEnd;
+    float m_fAnimFireTime;
+    float m_fAnim2LoopStart;
+    float m_fAnim2LoopEnd;
+    float m_fAnim2FireTime;
+    DWORD m_nModelID1;
+    DWORD m_nModelID2;
+};
+struct CObject
+{
+    DWORD vtable;
+    DWORD m_nObjectID;
+    CVector m_vecPosition;
+    float m_fScale;
+    bool m_bIsStatic;
+    // Tambahkan jika butuh (mis. m_pEntity)
 };
