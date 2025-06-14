f5d4f22957e0ebf919a0389345f10bbffee769f9
tezetzy <firlanrizqulloh@gmail.com>
2025-06-14 20:32:36 +0700

    Create Vectors.h

---
+++ Vectors.h
@@ -0,0 +1,28 @@
+#pragma once
+
+struct CVector
+{
+    float x, y, z;
+};
+
+struct CVector2D
+{
+    float x, y;
+};
+
+struct CIntVector2D
+{
+    int x, y;
+};
+
+struct CMatrix
+{
+    CVector right;
+    DWORD flags1;
+    CVector up;
+    DWORD flags2;
+    CVector at;
+    DWORD flags3;
+    CVector pos;
+    DWORD flags4;
+};
