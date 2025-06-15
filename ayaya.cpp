412826a24563e3bd6cf84955194e27c7a3e6fe05
tezetzy <firlanrizqulloh@gmail.com>
2025-06-15 14:10:43 +0700

    Create ayaya.cpp

---
+++ ayaya.cpp
@@ -0,0 +1,40 @@
+#include <mod/amlmod.h>
+#include <mod/logger.h>
+#include <mod/config.h>
+#include <dlfcn.h>
+#include <vector>
+#include <cctype>
+
+//#include "GTASA_Structur.h"
+#include "Weapons.h"
+#include "Peds.h"
+#include "Vehicles.h"
+#include "Vectors.h"
+#include "RenderWare.h"
+#include "Types.h"
+#include "PlayerInfo.h"
+
+MYMODCFG(net.jaymods.sfx, SpreadFix, 1.0, Jayzxy)
+
+uintptr_t hGTASA;
+float *fPlayerAimRotRate
+DECL_HOOK(bool, FireInstantHit, CWeapon *self, CEntity *a2, CVector *a3, CVector *a4, CEntity *a5, CVector *a6, CVector *a7, int a8, int a9)
+{
+    *fPlayerAimRotRate = (rand() * 2.0f * M_PI) / (float)RAND_MAX;
+    return FireInstantHit(self, a2, a3, a4, a5, a6, a7, a8, a9);
+}
+extern "C" void OnModLoad()
+{
+    logger->SetTag("SpreadFix");
+    hGTASA = dlopen("libGTASA.so", RTLD_LAZY);
+    cfg->Bind("Author", "", "About")->SetString("Jayzxy"); cfg->ClearLast();
+    cfg->Bind("IdeasFrom", "", "About")->SetString("MTA:SA Team, Peepo"); cfg->ClearLast();
+    cfg->Bind("Discord", "", "About")->SetString("dsc.gg/jaymods"); cfg->ClearLast();
+    cfg->Bind("GitHub", "", "About")->SetString("https://github.com/poretis/jaymods"); cfg->ClearLast();
+
+if(cfg->GetBool("WSFXV1JAY", true, "Gameplay"))
+    {
+       SET_TO(fPlayerAimRotRate,       aml->GetSym(hGTASA, "fPlayerAimRotRate"));
+       HOOK(FireInstantHit, aml->GetSym(hGTASA, "_ZN7CWeapon14FireInstantHitEP7CEntityP7CVectorS3_S1_S3_S3_bb"));
+    }
+}
