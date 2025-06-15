42852aa091a1d61709239595f0cf31a2ed4d00f0
tezetzy <firlanrizqulloh@gmail.com>
2025-06-15 13:08:12 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := AbtFixJ
-LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
+LOCAL_MODULE    := AbtStylePc
+LOCAL_SRC_FILES := StylePc.cpp mod/logger.cpp mod/config.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 LOCAL_C_INCLUDES += ./include
 LOCAL_LDLIBS += -llog #$(LOCAL_PATH)/libGLESv2.so
